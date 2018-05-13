a81e202de647125853e171910df99d95b8f54cd3
Romans Malinovskis <romans@agiletoolkit.org>
2018-05-13 10:57:26 +0100

    assignment 1 - addin Loan / Repayment models

---
+++ src/Model/Loan.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Model;
+
+class Loan extends \atk4\data\Model
+{
+    public $table = 'loan';
+    function init() {
+        parent::init();
+
+        $this->addFields([
+            ['date', 'type'=>'date'],
+            ['amount', 'type'=>'money'],
+        ]);
+
+        $this->hasOne('friend_id', new Friend());
+    }
+}
