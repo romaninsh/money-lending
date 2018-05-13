90e02127cf51c9d3ed260384ed080195183934d4
Romans Malinovskis <romans@agiletoolkit.org>
2018-05-13 08:54:28 +0100

    add basic files

---
+++ dashboard.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'vendor/autoload.php';
+
+require 'init.php';
+
+$app = new App();
+
+$app->add(['Message', 'The money-lending-app dashboard is not in use', 'info']);
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require 'vendor/autoload.php';
+
+require 'init.php';
+
+$app = new App();
+
+$app->add(['Message', 'The loan-app frontend is not in use', 'info']);
